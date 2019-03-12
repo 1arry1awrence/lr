62c927f1e95c7ed53fd125f025d384ff9198deee
1arry1awrence <691232093@qq.com>
2019-03-12 11:16:21 +0800

    init:初始化项目

---
+++ lr-action/src/main/java/com/zuanke8/lraction/LrActionApplication.java
@@ -0,0 +1,13 @@
+package com.zuanke8.lraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LrActionApplication.class, args);
+    }
+
+}
---
+++ lr-action/src/test/java/com/zuanke8/lraction/LrActionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zuanke8.lraction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LrActionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
